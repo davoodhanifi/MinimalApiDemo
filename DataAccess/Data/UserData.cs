78d8c3212d5eb300e831036a9e29306ecef82dcc
davood.hanifi <davood.hanifi@gmail.com>
2022-04-28 00:03:54 +0430

    Added data access layer with dapper.

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,29 @@
+﻿
+using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public class UserData
+{
+    private readonly ISqlDataAccess _dbAccess;
+
+    public UserData(ISqlDataAccess dbAccess)
+    {
+        _dbAccess = dbAccess;
+    }
+
+    public async Task<IEnumerable<UserModel>> GetAll() => await _dbAccess.LoadData<UserModel, dynamic>("spUser_GetAll", new { });
+
+    public async Task<UserModel> GetUser(int id)
+    {
+        var users = await _dbAccess.LoadData<UserModel, dynamic>("spUser_Get", new { Id = id });
+        return users.FirstOrDefault();
+    }
+
+    public async Task Insert(UserModel user) => await _dbAccess.SaveData("spUser_Insert", new { FirstName = user.FirstName, LastName = user.LastName });
+
+    public async Task Update(UserModel user) => await _dbAccess.SaveData("spUser_Update", user);
+
+    public async Task Delete(int id) => await _dbAccess.SaveData("spUser_Delete", id);
+}
---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.DbAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionString = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionString));
+
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T>(string storedProcedure, T parameters, string connectionString = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionString));
+
+        await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
