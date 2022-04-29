32d85d0cc6a2bcd29643dadbd0280cbb8dd9725b
davood.hanifi <davood.hanifi@gmail.com>
2022-04-30 00:46:17 +0430

    Added some tests.

---
+++ MinimalApiTests/IResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace MinimalApiTests;
+
+public static class IResultExtensions
+{
+    public static T? GetOkObjectResultValue<T>(this IResult result)
+    {
+        return (T?)Type.GetType("Microsoft.AspNetCore.Http.Result.OkObjectResult, Microsoft.AspNetCore.Http.Results")?.GetProperty("Value", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)?.GetValue(result);
+    }
+
+    public static int? GetObjectResultStatusCode(this IResult result)
+    {
+        return (int?)Type.GetType("Microsoft.AspNetCore.Http.Result.OkObjectResult, Microsoft.AspNetCore.Http.Results")?.GetProperty("StatusCode", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public)?.GetValue(result);
+    }
+}
---
+++ MinimalApiTests/UserEndpoitsTests.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Data;
+using DataAccess.Models;
+using MinimalApi.Endpoints;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace MinimalApiTests;
+
+public class UserEndpoitsTests
+{
+    private readonly IUserData _userData = Substitute.For<IUserData>();
+
+    [Fact]
+    public void GetUser_ReturnUser_WhenUserExists()
+    {
+        // Arrange
+        var id = new Random().Next();
+        var user = new UserModel { Id = id, FirstName = "Saul", LastName = "Goodman" };
+        _userData.GetUser(Arg.Is(id)).Returns(user);
+
+        // Act
+        var result = UserEndpoints.GetUser(_userData, id).Result;
+
+        // Assert
+        result.GetOkObjectResultValue<UserModel>().Equals(user);
+        result.GetObjectResultStatusCode().Equals(200);
+    }
+}
