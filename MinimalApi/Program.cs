cb116518e7b41bedde1cd8f49712e3ecdcee5bd0
davood.hanifi <davood.hanifi@gmail.com>
2022-04-29 14:04:48 +0430

    Added health checks.

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -5,13 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
+builder.Services.AddHealthChecks();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUserData, UserData>();
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 
 var app = builder.Build();
+app.MapHealthChecks("/healthz");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
